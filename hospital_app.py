ca85c021a30ca8a60952feff249fbf4c9d53d287
wisanggenipradipta-cmyk <wisanggenipradipta@gmail.com>
2026-07-02 13:28:08 +0700

    Create hospital_app.py

---
+++ hospital_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+st.set_page_config(
+    page_title="Smart Hospital Patient Navigator",
+    page_icon="🏥",
+    layout="wide"
+)
